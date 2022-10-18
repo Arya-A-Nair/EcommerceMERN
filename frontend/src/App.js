3ee48c3facbc5aed6b21f7a9141c8ae1e65f40c5
Killjoy <aryaajitnair@gmail.com>
2022-10-18 22:06:22 +0530

    List products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data";
 
 function App() {
-  return (
-    <div>
-      <header >
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        list products
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<a href="/">amazona</a>
+			</header>
+			<main>
+				<h1>Featured products</h1>
+				<div className="products">
+					{data.products.map((product) => (
+						<div key={product.slug} className="product">
+            <a href={`product/${product.slug}`}>
+							<img src={product.image} alt={product.name} />
+              </a>
+							<div className="product-info">
+								<p>{product.name}</p>
+								<p><strong>${product.price}</strong></p>
+                <button>Add to cart</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
