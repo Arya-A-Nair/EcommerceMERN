ad97d4b52aa2f7287e48ac98a26ebdce93a2be30
Killjoy <aryaajitnair@gmail.com>
2022-10-19 09:04:11 +0530

    Add reducer

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
+
+const reducer =(state,action)=>{
+	switch(action.type){
+		case 'FETCH_REQUEST':
+			return {...state,loading:true}
+		case 'FETCH_SUCCESS':
+			return {...state,products:action.payload,loading:false}
+		case 'FETCH_FAIL':
+			return {...state,loading:false,error:action.payload}
+		default:
+			return state
+	}
+}
+
 function HomeScreen() {
-	const [products,setProducts]=useState([])
+
+	const [{loading,error,products},dispatch]=useReducer(reducer,{
+		loading:true,
+		products:[],
+		error:''
+	})
+
+	// const [products,setProducts]=useState([])
 
 	useEffect(()=>{
+		dispatch({type:'FETCH_REQUEST'});
 		axios.get('/api/products')
 		.then((res)=>{
-			setProducts(res.data)
+			dispatch({type:'FETCH_SUCCESS',payload:res.data})
 		})
-		.catch(err=>console.log(err))
+		.catch(err=>{
+			dispatch({type:'FETCH_FAIL',payload:err.message})
+		})
 	},[])
 
   return (
