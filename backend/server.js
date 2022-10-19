ed9242c203fac00b1e9cfa818c946e83a7920883
Killjoy <aryaajitnair@gmail.com>
2022-10-19 23:18:17 +0530

    sign in API

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import data from './data.js'
 import 'dotenv/config'
 import mongoose from 'mongoose';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRouter.js';
+import userRouter from './routes/userRoutes.js';
 
 const db=mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log("DB connected")
@@ -12,11 +12,18 @@ const db=mongoose.connect(process.env.MONGODB_URI).then(()=>{
 })
 
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.use('/api/seed',seedRouter);
 
 
 app.use('/api/products',productRouter)
+app.use('/api/users',userRouter)
 
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+  });
 
 
 const port=process.env.PORT || 5000
