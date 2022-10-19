201242fd769520c7c814e49eaadc348c41253503
Killjoy <aryaajitnair@gmail.com>
2022-10-19 21:22:07 +0530

    Add product schema

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import data from './data.js'
 import 'dotenv/config'
 import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRouter.js';
 
 const db=mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log("DB connected")
@@ -10,34 +12,12 @@ const db=mongoose.connect(process.env.MONGODB_URI).then(()=>{
 })
 
 const app=express();
+app.use('/api/seed',seedRouter);
 
-app.get('/api/products',(req,res)=>{
-    res.send(data.products)
-})
 
-app.get('/api/products/slug/:slug',(req,res)=>{
-    const params=req.params
-    const {slug} =params
-    const product=data.products.find(x=>x.slug===slug)
-    if(product){
-        res.send(product)
-    }
-    else{
-        res.sendStatus(404)
-    }
-    
-})
+app.use('/api/products',productRouter)
 
-app.get('/api/products/:id',(req,res)=>{
-    const {id}=req.params
-    const product=data.products.find(x=>x._id===id)
-    if(product){
-        res.send(product)
-    }
-    else{
-        res.sendStatus(404)
-    }
-})
+
 
 const port=process.env.PORT || 5000
 
